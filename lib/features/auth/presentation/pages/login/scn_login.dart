1eb8cd247ca8cc85cc45c75e7e0a6a0a81be4339
Askhalan A <askhalan.aj@gmail.com>
2024-07-02 14:03:29 +0530

    Temp: checking git working

---
+++ lib/features/auth/presentation/pages/login/scn_login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScnLogin extends StatelessWidget {
+  const ScnLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
